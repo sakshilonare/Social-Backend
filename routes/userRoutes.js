f760e922a68cfb3361e625d341b1d3ffe2ee1bf2
sakshilonare <124262843+sakshilonare@users.noreply.github.com>
2025-01-19 15:30:49 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const { uploadUserData, getAllUsers } = require('../controller/userController');
+const express = require("express");
+const { uploadUserData, getAllUsers } = require("../controller/userController");
+const multer = require("multer");
+
 const router = express.Router();
 
-const multer = require('multer');
-const upload = multer({ dest: '../uploads/' }); 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // Routes
 router.post("/upload", upload.array("images", 10), uploadUserData); 
-router.get("/allUsers", getAllUsers); 
+router.get("/allUsers", getAllUsers);
 
 module.exports = router;
