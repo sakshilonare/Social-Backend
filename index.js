c8a8216e734b86b0fdcfc92708610b4e43cc2bf4
sakshilonare <124262843+sakshilonare@users.noreply.github.com>
2025-01-19 19:15:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRoutes); 
 app.use("/uploads", express.static("uploads"));
 
-app.listen(PORT, (./) => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
